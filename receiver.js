53f03c4e4717b8244c93359635c87d52a1d04e91
kerwin <327656123chw@163.com>
2024-04-21 17:23:37 +0800

    把微信的单独集中到一个判断里

--- receiver.js
+++ receiver.js
@@ -26,54 +26,56 @@ module.exports = RED => {
       if (config.platformType == "wechat") {
         const cryptor = new WXBizMsgCrypt(receiver_config.wechat_token, receiver_config.wechat_aeskey, receiver_config.wechat_corpid)
         const wx = new WeChat(node, receiver_config, cryptor)
-      }
-      const app = express()
-      // 接收消息主逻辑
-      app.all('/', async (req, res) => {
-        if (req.method == 'GET') {
-          // === 回调校验用 ==========
-          const sVerifyEchoStr = decodeURIComponent(req.query.echostr)
-          if (req.query.msg_signature == cryptor.getSignature(req.query.timestamp, req.query.nonce, sVerifyEchoStr)) {
-            res.send(cryptor.decrypt(sVerifyEchoStr).message)
-          } else {
-            res.status(200).send(indexHtml)
-          }
-        } else {
-          // === 正常通讯消息 ==========
-          try {
-            const message = await wx.getMessage(req)
-            console.log(`receive message: ${JSON.stringify(message)}`)
-            if (message.MsgType == 'voice' && receiver_config.client_id && receiver_config.client_id) {
-              const amr = await wx.getMedia(message.MediaId)
-              const asr = await bd.getAsr(amr)
-              message.AsrContent = asr
+        const app = express()
+        // 接收消息主逻辑
+        app.all('/', async (req, res) => {
+          if (req.method == 'GET') {
+            // === 回调校验用 ==========
+            const sVerifyEchoStr = decodeURIComponent(req.query.echostr)
+            if (req.query.msg_signature == cryptor.getSignature(req.query.timestamp, req.query.nonce, sVerifyEchoStr)) {
+              res.send(cryptor.decrypt(sVerifyEchoStr).message)
+            } else {
+              res.status(200).send(indexHtml)
             }
-            node.status({ text: `${message.MsgType}(${message.MsgId})` })
-            node.send({ res, req, config: receiver_config, message })
+          } else {
+            // === 正常通讯消息 ==========
+            try {
+              const message = await wx.getMessage(req)
+              console.log(`receive message: ${JSON.stringify(message)}`)
+              if (message.MsgType == 'voice' && receiver_config.client_id && receiver_config.client_id) {
+                const amr = await wx.getMedia(message.MediaId)
+                const asr = await bd.getAsr(amr)
+                message.AsrContent = asr
+              }
+              node.status({ text: `${message.MsgType}(${message.MsgId})` })
+              node.send({ res, req, config: receiver_config, message })
 
-            setTimeout(() => {
-              node.status({ text: `3秒超时自动应答`, fill: 'red', shape: 'ring' })
+              setTimeout(() => {
+                node.status({ text: `3秒超时自动应答`, fill: 'red', shape: 'ring' })
+                res.end('')
+              }, 3000)
+            } catch (err) {
+              node.status({ text: err.message, fill: 'red', shape: 'ring' })
+              node.warn(err)
               res.end('')
-            }, 3000)
-          } catch (err) {
-            node.status({ text: err.message, fill: 'red', shape: 'ring' })
-            node.warn(err)
-            res.end('')
+            }
           }
-        }
-      })
+        })
 
-      // 404
-      app.use((req, res, next) => {
-        res.status(404).end('')
-      })
+        // 404
+        app.use((req, res, next) => {
+          res.status(404).end('')
+        })
 
-      const server = app.listen(receiver_config.wechat_port, () => {
-        node.status({ text: `port: ${receiver_config.wechat_port}`, fill: 'green', shape: 'dot' })
-        node.log(`listening on port ${receiver_config.wechat_port}`)
-      })
-      server.on('error', ({ message }) => { node.status({ text: message, fill: 'red', shape: 'ring' }) })
-      node.on('close', () => server.close())
+        const server = app.listen(receiver_config.wechat_port, () => {
+          node.status({ text: `port: ${receiver_config.wechat_port}`, fill: 'green', shape: 'dot' })
+          node.log(`listening on port ${receiver_config.wechat_port}`)
+        })
+        server.on('error', ({ message }) => { node.status({ text: message, fill: 'red', shape: 'ring' }) })
+        node.on('close', () => server.close())
+
+      }
+
     }
   })
 }
