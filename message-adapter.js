31fed0c9d3269556f3af12d7a82531b95683e691
root <327656123chw@163.com>
2024-04-23 19:07:35 +0800

    完成适配器

--- message-adapter.js
+++ message-adapter.js
@@ -1,44 +1,56 @@
 // ======
+
+
 module.exports = RED => {
-  // 输入节点
-  RED.nodes.registerType('message_adapter', class {
-    constructor(config) {
-      this.on('input', function (msg, send, done) {
-        const message = {}
-        // do something with 'msg'
-        message.message_type = msg.message_type
-        if (msg.message_type == "wechat") {
+  function message_adapter(config) {
+    RED.nodes.createNode(this, config);
+    this.on('input', function (msg, send, done) {
+      const message = {}
+      // do something with 'msg'
+      //TODO
+      console.log(msg)
 
-          // TODO统一
-          message.message_id = msg.message.MsgId
-          message.content_type = msg.message.MsgType
-          message.content = msg.message.Content
 
-          message.from_user = msg.message.FromUserName
-          message.to_user = msg.message.ToUserName
-          message.room_id = null
+      message.message_type = msg.message_type
+      if (msg.message_type == "wechat") {
 
+        // TODO统一
+        message.message_id = msg.message.MsgId
+        message.content_type = msg.message.MsgType
+        message.content = msg.message.Content
+
+        message.from_user = msg.message.FromUserName
+        message.to_user = msg.message.ToUserName
+        message.chat_id = null
+
+      }
+      if (msg.message_type == "telegram") {
+
+        message.message_id = msg.message.message_id
+        if (msg.message.text != undefined) {
+          message.content_type = "text"
+          message.content = msg.message.text
         }
-        if (msg.message_type == "telegram") {
 
-          message.message_id = msg.message.MsgId
-          message.content_type = msg.message.MsgType
-          message.content = msg.message.Content
 
-          message.from_user = msg.message.FromUserName
-          message.to_user = msg.message.ToUserName
-          message.room_id = null
 
-        }
+        message.from_user = msg.message.from.id
+        message.to_user = null
+        message.chat_id = msg.message.chat.id
 
-        send(message)
-        // Once finished, call 'done'.
-        // This call is wrapped in a check that 'done' exists
-        // so the node will work in earlier versions of Node-RED (<1.0)
-        if (done) {
-          done();
-        }
-      });
-    }
-  })
+      }
+
+      send(message)
+      // Once finished, call 'done'.
+      // This call is wrapped in a check that 'done' exists
+      // so the node will work in earlier versions of Node-RED (<1.0)
+      if (done) {
+        done();
+      }
+    });
+  }
+
+  RED.nodes.registerType("message-adapter", message_adapter);
+
 }
+
--- receiver.js
+++ receiver.js
@@ -2,17 +2,27 @@ const { wechat_receiver } = require('./lib/wechat')
 const { telegram_receiver } = require('./lib/telegram')
 
 
-
 module.exports = RED => {
   // 输入节点
-  RED.nodes.registerType('receiver', class {
-    constructor(config) {
-      const node = this
-      RED.nodes.createNode(node, config)
-      const receiver_config = RED.nodes.getNode(config.botConfig)
+  // RED.nodes.registerType('receiver', class {
+  //   constructor(config) {
+  //     const node = this
+  //     RED.nodes.createNode(node, config)
+  //     const receiver_config = RED.nodes.getNode(config.botConfig)
 
-      receiver_config.platformType == "wechat" ? wechat_receiver(node, receiver_config) : null
-      receiver_config.platformType == "telegram" ? telegram_receiver(node, receiver_config) : null
-    }
-  })
+  //     receiver_config.platformType == "wechat" ? wechat_receiver(node, receiver_config) : null
+  //     receiver_config.platformType == "telegram" ? telegram_receiver(node, receiver_config) : null
+  //   }
+  // })
+  function receiver(config) {
+    RED.nodes.createNode(this, config)
+    const receiver_config = RED.nodes.getNode(config.botConfig)
+    receiver_config.platformType == "wechat" ? wechat_receiver(this, receiver_config) : null
+    receiver_config.platformType == "telegram" ? telegram_receiver(this, receiver_config) : null
+  }
+  RED.nodes.registerType("receiver", receiver);
 }
+
+
+
+
