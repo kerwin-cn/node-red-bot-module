5ce672e3555e61f3873c237b9c8da45744859273
root <327656123chw@163.com>
2024-04-25 17:51:28 +0800

    完成适配器，发送器的基础架构

--- message-adapter.js
+++ message-adapter.js
@@ -5,29 +5,26 @@ module.exports = RED => {
   function message_adapter(config) {
     RED.nodes.createNode(this, config);
     this.on('input', function (msg, send, done) {
-      const message = {}
-      // do something with 'msg'
-      //TODO
-      //console.log(msg)
+      const message = {
+        message_type: msg.message_type,
+        chat_id: null,
+        from_user: null,
+        content_type: null,
+        content: null,
+        ps: null
+      }
 
-
-      message.message_type = msg.message_type
 
       if (msg.message_type == "wechat") {
-        message.message_id = msg.message.MsgId
+        message.from_user = msg.message.FromUserName
         message.content_type = msg.message.MsgType
         message.content = msg.message.Content
-
-        message.from_user = msg.message.FromUserName
-        message.to_user = msg.message.ToUserName
-        message.chat_id = null
-        message.is_group = false
       }
 
 
       if (msg.message_type == "telegram") {
-        message.message_id = msg.message.message_id
-
+        message.chat_id = msg.message.chat.id
+        message.from_user = msg.message.from.id
 
         if ("text" in msg.message) {
           message.content_type = "text"
@@ -38,15 +35,7 @@ module.exports = RED => {
           message.content_type = "image"
           message.content = msg.message.photo
         }
-
-        message.from_user = msg.message.from.id
-        message.to_user = null
-        message.chat_id = msg.message.chat.id
-        message.is_group = msg.message.chat.id < 0 ? true : false
-
       }
-
-
 
       if (msg.message_type == "customize") {
         message = msg.message
--- message-sender.js
+++ message-sender.js
@@ -6,11 +6,19 @@ module.exports = RED => {
   function message_sender(config) {
     RED.nodes.createNode(this, config)
     const sender_config = RED.nodes.getNode(config.botConfig)
-
     this.on('input', function (msg, _, done) {
+      // 统一入口参数
+      send_message = {
+        message_type: msg.message_type,
+        chat_id: msg.chat_id,
+        to_user: msg.to_user,
+        content_type: msg.content_type,//必须有
+        content: msg.content,//必须有
+        ps: msg.ps
+      }
 
-      msg.message_type == "wechat" ? wechat_sender(this, sender_config, msg) : null
-      msg.message_type == "telegram" ? telegram_sender(this, sender_config, msg) : null
+      msg.message_type == "wechat" ? wechat_sender(sender_config, send_message) : null
+      msg.message_type == "telegram" ? telegram_sender(sender_config, send_message) : null
 
       if (done) {
         done();
